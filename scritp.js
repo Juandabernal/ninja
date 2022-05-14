f926a3eeba743206ef3a4c0743d2eb496a6c7ea6
Juan David Bernal Otalora <juandbernal@MacBook-Air-de-Juan.local>
2022-05-14 10:11:05 -0500

    [TAREA] sensei

--- scritp.js
+++ scritp.js
@@ -1,8 +1,6 @@
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
 
-class Ninja {
+
+class Ninja  {
     constructor (nombre, salud){ 
     this.nombre = nombre;
     this.salud = salud;
@@ -21,3 +19,34 @@ class Ninja {
         this.salud += 10;
     }
 }
+class Sensei extends Ninja {
+    constructor(nombre){
+        super (nombre, 200);
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.Sabiduria = 10;
+    }
+    speakWisdom () {
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+    }
+
+    drinkSake () {
+
+    }
+
+    showStats () {
+        console.log(this.nombre, this.velocidad, this.salud, this.fuerza);
+    }
+
+}
+const ninja1 = new Ninja("Hyabusa", 10);
+ninja1.sayName();
+ninja1.showStats();
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
